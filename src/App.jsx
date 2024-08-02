a398d38046434b8c1670b15fe0a16b385f7d4e4f
sanish <notsansh@outlook.com>
2024-08-02 17:05:03 +0545

    video7: Page not found

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import Home from './components/Home';
 import About from './components/About';
 import NavBar from './components/NavBar';
 import OrderSummary from './components/OrderSummary';
+import NoMatch from './components/NoMatch';
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-        {/*'Path' ko kura URL ma dekhinxa , 'element' ma chai page component pass garinxa*/}
+        {/* 'PATH' ko kura URL ma dekhinxa , 'ELEMENT' ma chai page component pass garinxa*/}
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='order-summary' element={<OrderSummary />} />
+        <Route path='*' element={<NoMatch />} />
       </Routes>
     </>
   );
