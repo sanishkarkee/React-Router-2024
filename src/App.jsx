1e5d5373e58789e3020e495937aed95c6262ebd7
sanish <notsansh@outlook.com>
2024-08-02 13:50:50 +0545

    video 4: Links

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
+        {/*'Path' ko kura URL ma dekhinxa , 'element' ma chai page component pass garinxa*/}
         <Route path='/' element={<Home />} />
-        {/*For index page, 'Path' ko kura URL ma dekhinxa , 'element' ma chai page component pass garinxa*/}
         <Route path='about' element={<About />} />
       </Routes>
     </>
